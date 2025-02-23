467bce1060652532da69c1b238f651b2da5d4c39
pbain63 <prodipbain63@gmail.com>
2025-02-23 21:14:18 +0600

    Add double for loop to select each cell in script.js

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 function Gameboard() {
-  const row = 3;
-  const column = 3;
+  const rows = 3;
+  const columns = 3;
   const board = [];
+
+  for (let i = 0; i < rows; i++) {
+    board[i] = [];
+    for (let j = 0; j < columns; j++) {
+      board[i].push(Cell());
+    }
+  }
 }
