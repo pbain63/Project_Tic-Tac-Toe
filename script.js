81cbe96b436687527ce69ee58786bc0b4ba8ca52
pbain63 <prodipbain63@gmail.com>
2025-02-24 18:19:28 +0600

    Add dropToken factory function to get available cells in script.js

--- script.js
+++ script.js
@@ -11,6 +11,14 @@ function Gameboard() {
   }
 
   const getBoard = () => board;
+
+  const dropToken = (column, player) => {
+    const availableCells = board
+      .filter((row) => row[column].getValue() === 0)
+      .map((row) => row[column]);
+
+    if (!availableCells.length) return;
+  };
 }
 
 function Cell() {
